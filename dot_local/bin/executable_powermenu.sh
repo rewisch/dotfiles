729600c32f9a831d1ef9c6089bcfe224158f87ef
Reto Wietlisbach <rwietlisbach@protonmail.com>
2026-02-13 21:43:55 +0100

    refactor waybar and also track powermenu-script

--- dot_local/bin/executable_powermenu.sh
+++ dot_local/bin/executable_powermenu.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-choice=$(printf "Shutdown\nReboot\nCancel" | rofi -dmenu -p "Power")
+choice=$(printf "Shutdown\nReboot\nSuspend" |
+  rofi -dmenu -p "Power" -i -lines 4 -columns 1 \
+    -theme-str 'listview { columns: 1; }')
 
 case "$choice" in
-  Shutdown)
-    systemctl poweroff
-    ;;
-  Reboot)
-    systemctl reboot
-    ;;
+Shutdown)
+  systemctl poweroff
+  ;;
+Reboot)
+  systemctl reboot
+  ;;
+Suspend)
+  systemctl suspend
+  ;;
 esac
